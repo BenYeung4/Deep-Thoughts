68cb6403580cee9e11cd1fc3887db0b19db9a680
Ben <byeungis@gmail.com>
2022-08-31 01:02:41 -0400

    set up Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import {
+  //ApolloProvider - special type of React component that we'll use to provide data to all the other components
+  ApolloProvider,
+  //ApolloClient - constructor function that will help initialize the connection to the GraphQL API server
+  ApolloClient,
+  //inMemoryCache - enables the Apollo Client instance to cache API response data so that we can preform requests more efficiently
+  InMemoryCache,
+  //createHttpLink - allows us to control how the Apollo Client makes a request.  "Ex. like middleware for the outbound network requests"
+  createHttpLink,
+} from "@apollo/client";
 
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+
+//established a new link in the GraphQL server at /graphql.  we can pass many other options and conficutaion setting with createHttpLink()
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+
+//using ApolloClient() constructor to instantiate the Apollo Client instance and create the connection to the API endpoint.
+const client = new ApolloClient({
+  link: httpLink,
+  //instigate new cache object using new InMemoryCache()
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    //ApolloProvider wrapped - passing the client variable in as the value for the client prop in the provider, everything between the JSX tags will eventually have access to the server's API data though the client we set up
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
